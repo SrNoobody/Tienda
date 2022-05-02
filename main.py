13019f44b70ba1b3b243b642795dd4c656b01f6a
Osvaldo Mazas <za18012229@zapopan.tecmm.edu.mx>
2022-05-02 17:51:09 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+"""Este es el inicio del proyecto a ejecutar en este se realizara una prueba de histograma
+utilizaremos unas cuantas variables que se generen aleatoriamente pro medio de nuestra
+funcion matematica o genrador de numero pseudoaleaotios. recordemos eque en este primer prototipo solo utilizaremos
+uno de los 3 productos mencionados en la introduccion de el proyecto
+"""
